47ccc3b2a48d6d42095d9cd0702427e358f08d7c
Andrew Necoechea <andrewnecoechea@gmail.com>
2026-09-10 22:14:30 -0400

    Style ticket table and status badges

--- TrackItUI/src/components/TicketCard.tsx
+++ TrackItUI/src/components/TicketCard.tsx
@@ -5,9 +5,18 @@ interface TicketCardProps {
 }
 
 function TicketCard({ ticket } : TicketCardProps) {
+  const date = new Date(ticket.createdAt)
   return (
     <div>
       <h2>{ticket.title}</h2>
+      <p>{ticket.status}</p>
+      <p>{ticket.description}</p>
+      <p>Created: {date.toLocaleDateString("en-US", {
+            month: "long",
+            day: "numeric",
+            year: "numeric",
+          })}
+      </p>
     </div>
   );
 }
--- TrackItUI/src/components/TicketList.tsx
+++ TrackItUI/src/components/TicketList.tsx
@@ -1,5 +1,5 @@
 import type { Ticket } from "../types/Ticket";
-import TicketCard from "./TicketCard";
+import "../styles/TicketList.css";
 
 interface TicketListProps {
   tickets: Ticket[];
@@ -7,11 +7,49 @@ interface TicketListProps {
 
 function TicketList({ tickets }: TicketListProps) {
   return (
-    <div>
-      {tickets.map((ticket) => (
-        <TicketCard key={ticket.id} ticket={ticket} />
-      ))}
-    </div>
+    <table className="ticket-table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Status</th>
+          <th>Asset</th>
+          <th>Created By</th>
+          <th>Assignee</th>
+          <th>Created</th>
+        </tr>
+      </thead>
+      
+      <tbody>
+        {tickets.map((ticket) => (
+          <tr key={ticket.id}>
+          <td>{ticket.id}</td>
+          <td>{ticket.title}</td>
+          <td>
+            <span className={`ticket-status ${ticket.status.toLowerCase().replace(" ", "-")}`}>
+              {ticket.status}
+            </span>
+          </td>
+          <td>{ticket.assetName}</td>
+          <td>{ticket.createdByUserName}</td>
+          <td>
+            {
+             ticket.assignees.map((assignee) =>
+              assignee.fullName)
+              .join(", ")            
+            }
+          </td>          
+          <td>
+            {new Date(ticket.createdAt).toLocaleDateString("en-US", {
+              month: "short",
+              day: "numeric",
+              year: "numeric",
+            })}
+          </td>            
+          </tr>
+        ))}
+      </tbody>
+    </table> 
   );
 }
 
